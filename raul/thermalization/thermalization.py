30f6c275e28965a2ebebeeb1c44d6d94b5093cfb
Raul Teixeira <teixeira@df.ufscar.br>
2026-02-17 16:43:09 +0100

    Study of thermalization for I = 1W

--- raul/thermalization/thermalization.py
+++ raul/thermalization/thermalization.py
@@ -15,7 +15,7 @@ k_max = 2*np.pi*0.5e3 # maximum k vector of the beam profile
 window = 8 * 2*np.pi/k_max
 puiss = 1.05
 Isat = 10e4  # saturation intensity in W/m^2
-L = 50e-3
+L = 500e-3
 alpha = 2
 
 def main():
@@ -31,7 +31,7 @@ def main():
         Isat=Isat,
         backend="GPU",
     )
-    simu.delta_z = 0.1e-4
+    simu.delta_z = 0.2e-4
     
     # Calculation of E_0 with a square distribution of k's, with random phase
     # it works for even NX=NY; not sure for odd.
@@ -54,11 +54,43 @@ def main():
     plt.imshow(abs(E_0))
     plt.savefig("img/input_beam.png")
 
-    A_plot = simu.out_field(E_0, L, verbose=True, plot=True, precision="single")
+    # parameters for callback
+    N_samples = 10
+    z_samples = np.zeros(N_samples+1) # +1 to account for the 0th step
+    z_samples[0] = 0
+    E_samples = np.zeros((N_samples+1, N, N), dtype=np.complex64)
+    E_0_normalized,_ = simu._prepare_output_array(E_0,True) 
+    E_samples[0] = E_0_normalized.get()
 
-    #obtain frequency content of field
-    E_out_fft = np.fft.fftshift(np.fft.fft2(A_plot))
+    N_steps = int(round(L/simu.delta_z))
+    save_every = N_steps//N_samples
 
+
+    def callback_sample(simu: NLSE, A: np.ndarray, z: float, i: int, E_samples: np.ndarray, z_samples: np.ndarray) -> None:
+        """A callback function for the NLSE class.
+
+        Args:
+            simu (NLSE): The simulation object
+            A (np.ndarray): The field at the current step
+            z (float): The current propagation distance in meters
+            i (int): The main loop index
+            E (np.ndarray): The array of samples
+        """
+        if i % save_every == 0:
+            E_samples[i//save_every+1] = A.get()
+            z_samples[i//save_every+1] = z
+ 
+    A_plot = simu.out_field(E_0, L, callback=callback_sample, callback_args=(E_samples, z_samples), verbose=True)
+    
+    #A_plot = simu.out_field(E_0, L, verbose=True, plot=True, precision="single")
+
+    #obtain frequency content of fields
+    E_fft = np.zeros([N_samples+1,A_plot.shape[0],A_plot.shape[1]])
+    E_fft[:] = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(E_samples[:]))).astype(
+        PRECISION_COMPLEX
+    )
+
+    E_out_fft = E_fft[N_samples]
     plt.figure()
     plt.imshow(abs(E_out_fft))
     #simu.plot_field(A_plot,L,True,"img/Output2.png")
@@ -67,19 +99,42 @@ def main():
     y, x = np.indices(E_out_fft.shape)
     r = np.sqrt((x - zero_index)**2 + (y - zero_index)**2)
     r = r.astype(np.int32)  # bin by integer radius
-
-    E_out_fft_abs = abs(E_out_fft)
-    
-    # Sum values in each radial bin
-    tbin = np.bincount(r.ravel(), E_out_fft_abs.ravel())
     nr = np.bincount(r.ravel())
     
-    radial_mean = tbin / nr
+    E_fft_abs = abs(E_fft)
+    
+    radial_mean_fft = np.zeros([N_samples+1,nr.shape[0]])
 
-    k_r = np.arange(len(radial_mean))*k_step
+    # Sum values in each radial bin
+    for ii in np.arange(N_samples+1):
+        E_out_fft_abs = E_fft_abs[ii]
+        tbin = np.bincount(r.ravel(), E_out_fft_abs.ravel())
+        radial_mean_fft[ii] = tbin / nr
+
+    #print(radial_mean_fft[10])
+    
+    k_r = np.arange(len(nr))*k_step
+    k_max_plot = 200*k_max
+    plot_index = int(k_max_plot/k_step)
     plt.figure()
-    plt.semilogy(k_r,radial_mean)
+    for ii in np.arange(N_samples):
+        plt.loglog(k_r[0:plot_index],radial_mean_fft[ii,0:plot_index])
+
+    exponent = -8
+    in_min = 20
+    in_max = 100
+    plt.loglog(k_r[in_min:in_max],(k_r[in_min:in_max]/k_max/10)**exponent)
     plt.savefig("img/azimuthal_fft_out.png")
+
+    plt.figure()
+    plt.imshow(abs(E_samples[10]))
+    plt.savefig("img/output_beam.png")
+
+
+    plt.figure()
+    for ii in np.arange(N_samples):
+        plt.semilogy(k_r,radial_mean_fft[ii])
+    plt.savefig("img/azimuthal_fft_out_1.png")
 
 if __name__ == "__main__":
     main()
